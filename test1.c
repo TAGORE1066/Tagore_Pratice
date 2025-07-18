3df70d7e70c11af24c07a6afdf521e2ab6436209
Your Name <you@example.com>
2025-07-18 17:44:33 +0530

    Arithmetic

---
+++ test1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	int a = 25;
+	int b = 58;
+	int c;
+	printf("Addition of value c:%d\n Division of value c:%d\n multiplication of value c:%d\n Subtraction of value c:%d\n",a-b,a/b,a*b,a-b);
+	return 0;
+}
