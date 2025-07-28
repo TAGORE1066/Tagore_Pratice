8ecd527bd288096f757e4dc4a5451240071127f6
Your Name <you@example.com>
2025-07-28 11:51:20 +0530

    operators

---
+++ logical.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int a = 12;
+
+if ( a == 12 && a != 13 && a <= 13 && a >= 12)
+{
+printf("The true value's of: a == 12%d\n a != 13%d\n a <= 13%d\n a >= 13%d\n",a ==12,a != 13,a <= 13,a >=12);
+}
+return 0;
+}
+
