6d84878ce9d60e2e2c57a734590efbbbc3701cde
Your Name <you@example.com>
2025-07-28 17:12:26 +0530

    Area of rectangle

---
+++ questions/5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+int length;
+int breadth;
+int area;
+
+printf("Area of a rectangle:");
+scanf("%d %d",&length,&breadth);
+area = length * breadth;
+printf("Area of a rectangle:%d\n",length*breadth);
+
+return 0;
+
+}
+
