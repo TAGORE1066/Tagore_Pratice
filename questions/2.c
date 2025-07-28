ac6cc98f451475524fbe6b33299778a4a4e5a3e3
Your Name <you@example.com>
2025-07-28 16:30:45 +0530

    sum of two numbers

---
+++ questions/2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int  main(void)
+{
+
+int a;
+int b;
+int c;
+
+printf("sum of two numbers: ");
+scanf("%d %d",&a,&b);
+c = a + b;
+printf("Adding two numbers:%d\n",c);
+return 0;
+}
