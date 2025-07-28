1391004d12d529c6b806d58c37123dd3b8b4ded1
Your Name <you@example.com>
2025-07-28 16:58:28 +0530

    arthimetic operations

---
+++ questions/3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+
+int a;
+int b;
+int c;
+
+
+printf("Addition of  the numbers: ");
+scanf("%d %d",&a,&b);
+c = a + b;
+printf("Addition of two numbers: %d\n",c = a + b);
+
+
+printf("Subtraction of  the numbers: ");
+scanf("%d %d",&a,&b);
+c = a - b;
+printf("subtraction of two numbers:%d\n",c = a - b);
+
+printf("Multiplication of  the numbers: ");
+scanf("%d %d",&a,&b);
+c = a * b;
+printf("multiplication of two numbers:%d\n",c = a * b);
+
+printf("Division of  the numbers: ");
+scanf("%d %d",&a,&b);
+c = a / b;
+printf("division of two numbers:%d\n",c = a / b);
+
+printf("Modulous the numbers: ");
+scanf("%d %d",&a, &b);
+c = a % b;
+printf("modulous of two numbers:%d\n",c = a % b);
+
+return 0;
+
+}
+
