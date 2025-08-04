a23ecc87fbb57d26ca3618f218c141e82c7ca945
Your Name <you@example.com>
2025-08-04 11:31:22 +0530

    ifelse statement

---
+++ conditionalstatements/ifelse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+int num;
+printf("Enter the number:");
+scanf("%d",&num);
+
+if(num %2 == 0)
+{
+printf("number is even.\n");
+}
+else
+{
+printf("number is odd.\n");
+}
+return 0;
+}
