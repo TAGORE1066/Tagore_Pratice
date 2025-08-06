b4f57311df46556f765177bbe90db6c5e734edbb
Your Name <you@example.com>
2025-08-06 11:41:30 +0530

    SquareRoot of a number

---
+++ questions/12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+{
+
+float a,root;
+
+printf("Enter the number:");
+scanf("%f",&a);
+
+root = sqrt(a);
+
+printf("Square root of a number:%.2f\n",root);
+
+return 0;
+
+}
