58c5139b7f054e7150542370dc469ef7602f1793
Your Name <you@example.com>
2025-08-06 11:48:26 +0530

    Power of a number

---
+++ questions/11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+int a,power;
+
+printf("Enter the number:");
+scanf("%d",&a);
+
+power = pow(a, 2);
+
+printf("Power of a number:%d\n",power);
+
+return 0;
+
+}
+
+
