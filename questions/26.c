7969256667ccb71efabd71a45b9c8a51cbef337e
Your Name <you@example.com>
2025-08-06 10:50:01 +0530

    Difference between two numbers

---
+++ questions/26.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+int a,b,difference;
+
+printf("Enter the numbers:");
+scanf("%d %d",&a,&b);
+
+difference = a - b;
+
+printf("Difference b/w two numbers:%d\n",difference);
+
+return 0;
+
+}
