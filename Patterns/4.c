afd382f29d9b6cf9a0c0499ad49225608b6eb594
Your Name <you@example.com>
2025-08-13 11:27:21 +0530

    Invert left half pyramid

---
+++ Patterns/4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int i,j,k;
+for(i = 0; i < 10; i++)
+{
+for(j = 0; j < 2*i; j++)
+{
+printf(" ");
+}
+for(k = 0; k < 10-i; k++)
+{
+printf("* ");
+}
+printf("\n");
+}
+return 0;
+}
+
