0e2a9bce829d3afdfe749cfbef1b1ce40a9da71c
Your Name <you@example.com>
2025-09-04 18:34:41 +0530

    Comparision of NULL

---
+++ pointers/NULL.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+int *ptr = NULL;
+
+if(ptr == NULL)
+{
+printf("The pointer is NULL\n");
+}
+else
+{
+printf("The pointer is not NULL\n");
+}
+
+return 0;
+
+}
+
