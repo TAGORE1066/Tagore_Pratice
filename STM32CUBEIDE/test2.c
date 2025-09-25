519fa91ce8af7bb11721fff0c633cd52ab5e7f60
Your Name <you@example.com>
2025-09-25 17:35:15 +0530

    Led-Blinking with Button

---
+++ STM32CUBEIDE/test2.c
@@ -0,0 +1,28 @@
+unsigned int* RCC_AHB1ENR=(unsigned int *)0x40023830;
+unsigned int* GPIOC_MODER=(unsigned int *)0x40020800;
+unsigned int* GPIOC_ODR=(unsigned int *)0x40020814;
+unsigned int* GPIOC_IDR=(unsigned int*)0x40020810;
+
+void delay(long delay)
+{
+	while(delay--);
+}
+int main()
+{
+	*RCC_AHB1ENR |=(1<<2);
+	*GPIOC_MODER |=(1<<26);
+	*GPIOC_MODER &=~(3 << 26);
+	*GPIOC_MODER &=~(3<<30);
+
+	while(1)
+	{
+		if((*GPIOC_IDR &(1<<15))==0)
+			*GPIOC_ODR |=(1<<13);
+		else
+			*GPIOC_ODR &=~(1<<13);
+	}
+	delay(500000);
+	return 0;
+
+}
+
